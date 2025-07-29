6c1c3bba33b6ab3a92dc886e88812c8533265065
mfx1000 <anikaitsinghdev989@gmail.com>
2025-07-29 22:45:54 +0530

    Refactor: moved client to root and fixed import paths

--- vite.config.ts
+++ vite.config.ts
@@ -78,6 +78,38 @@
 // });
 
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+
+// // ⬇️ For __dirname workaround
+// const __dirname = path.dirname(new URL(import.meta.url).pathname.replace(/^\/([A-Z]:)/, '$1'));
+
+// export default defineConfig({
+//   plugins: [react(), runtimeErrorOverlay()],
+//   resolve: {
+//     alias: {
+//       // "@": path.resolve(__dirname, "src"),
+//       "@": path.resolve(__dirname, "client", "src"),
+//       "@shared": path.resolve(__dirname, "shared"),
+//       "@assets": path.resolve(__dirname, "attached_assets"),
+//     },
+//   },
+//   // root: __dirname,
+//   root: path.resolve(__dirname, "client"),
+//   build: {
+//     outDir: path.resolve(__dirname, "dist/public"),
+//     emptyOutDir: true,
+//   },
+//   server: {
+//     fs: {
+//       strict: true,
+//     },
+//   },
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -97,9 +129,9 @@ export default defineConfig({
     },
   },
   // root: __dirname,
-  root: path.resolve(__dirname, "client"),
+  // root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "public"),
     emptyOutDir: true,
   },
   server: {
